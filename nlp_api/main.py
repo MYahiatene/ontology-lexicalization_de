db25d63386e5b2ad49d7095a3c82b250c1c3f04f
Myahiatene <myahiatene@itemis.com>
2022-10-19 22:08:46 +0200

    refactoring of stanza api

--- nlp_api/main.py
+++ nlp_api/main.py
@@ -1,17 +1,7 @@
 from fastapi import FastAPI
 import stanza
-import spacy_stanza
+# import spacy_stanza
 from pydantic import BaseModel
-from fastapi.logger import logger
-import logging
-
-gunicorn_logger = logging.getLogger('gunicorn.error')
-logger.handlers = gunicorn_logger.handlers
-if __name__ != "main":
-    logger.setLevel(gunicorn_logger.level)
-else:
-    logger.setLevel(logging.DEBUG)
-# Download the stanza model if necessary
 
 # Initialize the pipeline
 nlp = None
@@ -31,17 +21,13 @@ def info():
 
 @app.get("/{lang}")
 async def read_root(lang: str) -> str:
-    stanza.download(lang)
     global nlp
-    nlp = spacy_stanza.load_pipeline(lang)
+    nlp = stanza.Pipeline(lang, processors='tokenize,pos,lemma', verbose=True)
     return "Language {lang} downloaded and initialized.".format(lang=lang)
 
 
 @app.post("/text")
 async def read_item(text: Text):
     doc = nlp(text.text)
-    nlp_list = []
-    for token in doc:
-        nlp_list.append([token.text, token.lemma_, token.pos_])
-    logger.error(nlp_list)
-    return nlp_list
+    return [[word.text, word.lemma, word.pos]
+            for sent in doc.sentences for word in sent.words]
